f3162d950147db32647d555a40d32aacf606df72
Miron <popovmiron@gmail.com>
2024-09-29 21:30:02 +0700

    Константы URI для приложения

---
+++ app/src/main/java/com/sekvenia/movie/common/Constans.kt
@@ -0,0 +1,6 @@
+package com.sekvenia.movie.common
+
+object Constans {
+    const val BASE_URL = "https://s3-eu-west-1.amazonaws.com/"
+    const val ERROR_IMAGE = "https://st.kp.yandex.net/images/no-poster.gif"
+}
