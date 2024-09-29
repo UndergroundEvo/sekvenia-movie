05563cca2e6e017d98c7434e1b427554db8eb79b
Miron <popovmiron@gmail.com>
2024-09-29 21:19:56 +0700

    Api интерфейс для получения данных о фильмах

---
+++ app/src/main/java/com/sekvenia/movie/data/network/CinemaApi.kt
@@ -0,0 +1,10 @@
+package com.sekvenia.movie.data.network
+
+import com.sekvenia.movie.data.models.CinemaDto
+import retrofit2.http.GET
+
+interface CinemaApi {
+
+    @GET("sequeniatesttask/films.json")
+    suspend fun getMovies() : CinemaDto
+}
