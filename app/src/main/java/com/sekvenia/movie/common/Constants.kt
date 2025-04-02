d7a1b3d749b01b222776d7831324e1873a220ea5
Miron <popovmiron@gmail.com>
2025-04-02 18:23:27 +0700

    Лишняя ссылка в константах

--- app/src/main/java/com/sekvenia/movie/common/Constants.kt
+++ app/src/main/java/com/sekvenia/movie/common/Constants.kt
@@ -1,6 +1,5 @@
 package com.sekvenia.movie.common
 
-object Constans {
+object Constants {
     const val BASE_URL = "https://s3-eu-west-1.amazonaws.com/"
-    const val ERROR_IMAGE = "https://st.kp.yandex.net/images/no-poster.gif"
 }
