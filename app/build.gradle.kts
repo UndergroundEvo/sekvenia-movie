7b66becd8bed16feb8f2de25fe9c48f24bdc7da8
Miron <popovmiron@gmail.com>
2025-04-02 22:03:38 +0700

    Изменение в gradle, зафиксировал версии библиотек

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,6 +80,11 @@ dependencies {
     // для асинхронной загрузки изображений
     implementation(libs.coil.compose)
 
+    testImplementation (libs.junit.jupiter.api)
+    testImplementation (libs.mockk)
+    testImplementation (libs.kotlinx.coroutines.test)
+    testImplementation (libs.androidx.core.testing)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -89,10 +94,12 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     testImplementation(libs.junit)
+    testImplementation(libs.junit.jupiter)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    testImplementation(kotlin("test"))
 }
